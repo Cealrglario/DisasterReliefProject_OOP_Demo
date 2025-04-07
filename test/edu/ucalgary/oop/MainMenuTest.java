3012458fe7ecd32b9f57b61232e1c27a389fb832
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-04-07 15:02:51 -0600

    Finish MainMenu test

--- test/edu/ucalgary/oop/MainMenuTest.java
+++ test/edu/ucalgary/oop/MainMenuTest.java
@@ -2,13 +2,24 @@ package edu.ucalgary.oop;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class MainMenuTest {
+    private MainMenu testMenu;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        String[] testDefaultOptions = {"1. Manage Persons", "2. Manage Locations", "3. Manage Inquiries",
+        "4. Manage Family Groups", "5. Manage Supplies"};
+
+        testMenu = new MainMenu(testDefaultOptions);
     }
 
     @Test
     public void testMainMenuConstructor() {
+        String[] testDefaultOptions = {"1. Manage Persons", "2. Manage Locations", "3. Manage Inquiries",
+                "4. Manage Family Groups", "5. Manage Supplies"};
+
+        assertEquals("MainMenu options should be set as expected", testDefaultOptions, testMenu.getDefaultOptions());
     }
 }
