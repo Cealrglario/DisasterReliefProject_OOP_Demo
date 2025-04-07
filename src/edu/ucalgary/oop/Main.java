2eade18c4eb6f6737b30c7048ab89646bb41cea0
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-04-07 13:23:33 -0600

    Update translation initialization in Main

--- src/edu/ucalgary/oop/Main.java
+++ src/edu/ucalgary/oop/Main.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void Main(String[] args) {
         Scanner userInput = new Scanner(System.in);
+        String selectedLanguage;
         LanguageManager languageManager = LanguageManager.INSTANCE;
         MenuManager menuManager = MenuManager.INSTANCE;
 
         System.out.println("Please select a language (for example, type en-CA for Canadian English): ");
+        selectedLanguage = userInput.nextLine();
 
-        languageManager.setCurrentLanguage(userInput.nextLine());
+        if(!languageManager.configureLanguage(selectedLanguage)) {
+            System.exit(1);
+        }
 
         userInput.close();
 
