5e3305aeadd7815457b46e381273cc6ed2fef007
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-04-07 11:53:45 -0600

    Begin coding menu navigation

--- src/edu/ucalgary/oop/MenuManager.java
+++ src/edu/ucalgary/oop/MenuManager.java
@@ -6,21 +6,37 @@ public enum MenuManager {
     INSTANCE;
 
     private static boolean isRunning;
+    private static Menu currentMenu;
     private static Stack<Menu> menuHistory;
 
     public void initializeMenuLanguage() {}
 
-    public void navigateToMenu(Menu menu) {}
+    public void navigateToMenu(Menu menu) {
+        menuHistory.push(menu);
+        currentMenu = menu;
+    }
 
-    public void backtrackMenus() {}
+    public void backtrackMenus() {
+        menuHistory.pop();
+        currentMenu = menuHistory.peek();
+    }
+
+    public void startRunning() {
+        isRunning = true;
+    }
 
     public boolean getIsRunning() {
         return isRunning;
     }
 
-    public void exit() {}
+    public void stopRunning() {
+        isRunning = false;
+    }
 
-    public void run() {}
+    public void run() {
+        currentMenu.handleDisplay();
+        currentMenu.handleInput();
+    }
 
 }
 
