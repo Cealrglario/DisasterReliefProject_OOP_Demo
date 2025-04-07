33066d2adcce4e13669207788fa96b97af54c4db
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-04-07 11:53:33 -0600

    Begin developing program entry point

--- src/edu/ucalgary/oop/Main.java
+++ src/edu/ucalgary/oop/Main.java
@@ -3,5 +3,22 @@ package edu.ucalgary.oop;
 import java.util.Scanner;
 
 public class Main {
-    public static void Main(String[] args) {}
+    public static void Main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        LanguageManager languageManager = LanguageManager.INSTANCE;
+        MenuManager menuManager = MenuManager.INSTANCE;
+
+        System.out.println("Please select a language (for example, type en-CA for Canadian English): ");
+
+        languageManager.setCurrentLanguage(userInput.nextLine());
+
+        System.out.println("The language you selected is: " + languageManager.getCurrentLanguage());
+
+        menuManager.initializeMenuLanguage();
+        menuManager.startRunning();
+
+        while(menuManager.getIsRunning()) {
+            menuManager.run();
+        }
+    }
 }
