bba44cd174a218fbccbd24a154129d075b16a834
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-04-10 12:06:22 -0600

    Finish Location and MedicalRecord Access

--- src/edu/ucalgary/oop/LocationAccess.java
+++ src/edu/ucalgary/oop/LocationAccess.java
@@ -1,6 +1,109 @@
 package edu.ucalgary.oop;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocationAccess<U> extends DatabaseObjectAccess<Location, U> {
+
+    @Override
+    public List<Location> getAll() throws SQLException {
+        List<Location> retrievedLocations = new ArrayList<>();
+
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        Statement myStmt = dbConnect.createStatement();
+        queryResults = myStmt.executeQuery("SELECT * FROM Location");
+
+        while(queryResults.next()) {
+            int locationId = queryResults.getInt("location_id");
+            String name = queryResults.getString("name");
+            String address = queryResults.getString("address");
+
+            Location retrievedLocation = new Location(locationId, name, address);
+            retrievedLocations.add(retrievedLocation);
+        }
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        return retrievedLocations;
+    }
+
+
+    @Override
+    public Location getById(int idOfLocation) throws SQLException {
+        Location retrievedLocation = null;
+
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        Statement myStmt = dbConnect.createStatement();
+        queryResults = myStmt.executeQuery("SELECT * FROM Location WHERE location_id = " + idOfLocation);
+
+        if(queryResults.next()) {
+            int locationId = queryResults.getInt("location_id");
+            String name = queryResults.getString("name");
+            String address = queryResults.getString("address");
+
+            retrievedLocation = new Location(locationId, name, address);
+        } else {
+            throw new SQLException("Error getting Location by ID: Location doesn't exist.");
+        }
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        return retrievedLocation;
+    }
+
+
+    @Override
+    public boolean updateInfo(String infoToUpdate, U newInfo, int locationId) throws SQLException {
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        PreparedStatement myStmt = dbConnect.prepareStatement("UPDATE Location SET " + infoToUpdate + " = " + newInfo +
+                " WHERE location_id = ?");
+
+        myStmt.setInt(1, locationId);
+
+        int affectedRows = myStmt.executeUpdate();
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        if (affectedRows == 0) {
+            System.out.println("Updating location failed, no rows affected.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    @Override
+    public U getInfo(String infoToGet, int locationId) throws SQLException {
+        U retrievedInfo;
+
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        Statement myStmt = dbConnect.createStatement();
+        queryResults = myStmt.executeQuery("SELECT " + infoToGet + "FROM Location WHERE location_id = " +
+                locationId);
+
+        if(queryResults.next()) {
+            retrievedInfo = (U) queryResults.getObject(infoToGet);
+        } else {
+            System.out.println("Error retrieving info, results empty.");
+            return null;
+        }
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        return retrievedInfo;
+    }
 }
--- src/edu/ucalgary/oop/MedicalRecordAccess.java
+++ src/edu/ucalgary/oop/MedicalRecordAccess.java
@@ -1,6 +1,162 @@
 package edu.ucalgary.oop;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MedicalRecordAccess<U> extends DatabaseObjectAccess<MedicalRecord, U> {
+
+    @Override
+    public List<MedicalRecord> getAll() throws SQLException {
+        List<MedicalRecord> retrievedMedicalRecords = new ArrayList<>();
+
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        Statement myStmt = dbConnect.createStatement();
+        queryResults = myStmt.executeQuery("SELECT * FROM MedicalRecord");
+
+        while(queryResults.next()) {
+            int medicalRecordId = queryResults.getInt("medical_record_id");
+            int locationId = queryResults.getInt("location_id");
+            String treatmentDetails = queryResults.getString("treatment_details");
+
+            MedicalRecord retrievedMedicalRecord = new MedicalRecord(medicalRecordId, locationId, treatmentDetails);
+            retrievedMedicalRecords.add(retrievedMedicalRecord);
+        }
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        return retrievedMedicalRecords;
+    }
+
+
+    @Override
+    public MedicalRecord getById(int idOfMedicalRecord) throws SQLException {
+        MedicalRecord retrievedMedicalRecord = null;
+
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        Statement myStmt = dbConnect.createStatement();
+        queryResults = myStmt.executeQuery("SELECT * FROM MedicalRecord WHERE medical_record_id = " + idOfMedicalRecord);
+
+        if(queryResults.next()) {
+            int medicalRecordId = queryResults.getInt("medical_record_id");
+            int locationId = queryResults.getInt("location_id");
+            String treatmentDetails = queryResults.getString("treatment_details");
+
+            retrievedMedicalRecord = new MedicalRecord(medicalRecordId, locationId, treatmentDetails);
+        } else {
+            throw new SQLException("Error getting MedicalRecord by ID: MedicalRecord doesn't exist.");
+        }
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        return retrievedMedicalRecord;
+    }
+
+
+    public MedicalRecord addMedicalRecord(int locationId, String treatmentDetails) throws SQLException {
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        PreparedStatement myStmt = dbConnect.prepareStatement(
+                "INSERT INTO MedicalRecord (location_id, treatment_details) VALUES (?, ?)",
+                Statement.RETURN_GENERATED_KEYS
+        );
+
+        myStmt.setInt(1, locationId);
+        myStmt.setString(2, treatmentDetails);
+
+        int affectedRows = myStmt.executeUpdate();
+        if (affectedRows == 0) {
+            throw new SQLException("Creating medical record failed, no rows affected.");
+        }
+
+        MedicalRecord newMedicalRecord = null;
+        try (ResultSet generatedKeys = myStmt.getGeneratedKeys()) {
+            if (generatedKeys.next()) {
+                int medicalRecordId = generatedKeys.getInt(1);
+                newMedicalRecord = new MedicalRecord(medicalRecordId, locationId, treatmentDetails);
+            } else {
+                throw new SQLException("Creating medical record failed, couldn't obtain medical record ID.");
+            }
+        }
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        return newMedicalRecord;
+    }
+
+
+    @Override
+    public boolean updateInfo(String infoToUpdate, U newInfo, int medicalRecordId) throws SQLException {
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        PreparedStatement myStmt = dbConnect.prepareStatement("UPDATE MedicalRecord SET " + infoToUpdate + " = " + newInfo +
+                " WHERE medical_record_id = ?");
+
+        myStmt.setInt(1, medicalRecordId);
+
+        int affectedRows = myStmt.executeUpdate();
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        if (affectedRows == 0) {
+            System.out.println("Updating medical record failed, no rows affected.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    @Override
+    public U getInfo(String infoToGet, int medicalRecordId) throws SQLException {
+        U retrievedInfo;
+
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        Statement myStmt = dbConnect.createStatement();
+        queryResults = myStmt.executeQuery("SELECT " + infoToGet + "FROM MedicalRecord WHERE medical_record_id = " +
+                medicalRecordId);
+
+        if(queryResults.next()) {
+            retrievedInfo = (U) queryResults.getObject(infoToGet);
+        } else {
+            System.out.println("Error retrieving info, results empty.");
+            return null;
+        }
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        return retrievedInfo;
+    }
+
+
+    public boolean removeMedicalRecord(MedicalRecord unwantedMedicalRecord) throws SQLException {
+        int unwantedMedicalRecordId = unwantedMedicalRecord.getMedicalRecordId();
+
+        dbConnectionManager.initializeDbConnection();
+        Connection dbConnect = dbConnectionManager.getDbConnection();
+
+        PreparedStatement myStmt = dbConnect.prepareStatement("DELETE FROM MedicalRecord WHERE medical_record_id = ?");
+
+        myStmt.setInt(1, unwantedMedicalRecordId);
+
+        int rowsAffected = myStmt.executeUpdate();
+
+        myStmt.close();
+        dbConnectionManager.closeDbConnection();
+
+        return rowsAffected > 0;
+    }
 }
