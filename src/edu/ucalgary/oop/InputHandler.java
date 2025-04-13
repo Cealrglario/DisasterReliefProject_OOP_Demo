f87e0888e493f6ccd1ffd4f53d6dc4309b47e7f9
Carl Elgario <kirstencarl.elgario@ucalgary.ca>
2025-04-13 11:24:50 -0600

    Add more robust input validation to InputHandler

--- src/edu/ucalgary/oop/InputHandler.java
+++ src/edu/ucalgary/oop/InputHandler.java
@@ -6,7 +6,18 @@ public class InputHandler {
     private final Scanner INPUT_SCANNER = new Scanner(System.in);
 
     public int getIntInput(int min, int max) {
-        int userInput = INPUT_SCANNER.nextInt();
+        Integer userInput = null;
+        boolean intInput = false;
+
+        while(!intInput) {
+            try {
+                userInput = INPUT_SCANNER.nextInt();
+                intInput = true;
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please select an option by typing in its corresponding number: ");
+                userInput = INPUT_SCANNER.nextInt();
+            }
+        }
 
         while(userInput < min || userInput > max) {
             System.out.println("Invalid input. Please select an option by typing in its corresponding number: ");
@@ -18,15 +29,20 @@ public class InputHandler {
 
     public String getStringInput(boolean emptyAllowed, boolean numbersAllowed) {
         String userInput = INPUT_SCANNER.nextLine();
+        boolean validInput = false;
 
-        while(!emptyAllowed && userInput.isEmpty()) {
-            System.out.println("Invalid input. Empty inputs are not allowed here. Try again: ");
-            userInput = INPUT_SCANNER.nextLine();
-        }
+        while(!validInput) {
+            while(!emptyAllowed && userInput.isEmpty()) {
+                System.out.println("Invalid input. Empty inputs are not allowed here. Try again: ");
+                userInput = INPUT_SCANNER.nextLine();
+            }
 
-        while(!numbersAllowed && containsDigit(userInput)) {
-            System.out.println("Invalid input. Numbers in your input are not allowed here. Try again: ");
-            userInput = INPUT_SCANNER.nextLine();
+            while(!numbersAllowed && containsDigit(userInput)) {
+                System.out.println("Invalid input. Numbers in your input are not allowed here. Try again: ");
+                userInput = INPUT_SCANNER.nextLine();
+            }
+
+            validInput = true;
         }
 
         return userInput;
